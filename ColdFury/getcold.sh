ac28a8191c4ef511409e89d86d32e19464c1ad3a
Bob Swanson <40324245+wwone@users.noreply.github.com>
2018-12-03 15:03:56 -0700

    Update getcold.sh
    Added information

--- ColdFury/getcold.sh
+++ ColdFury/getcold.sh
@@ -24,6 +24,10 @@ OBJ=ReadXMLDOM
 # depends on the presence in this directory of the
 # "ReadXMLDOM.class" file, and the "java" runtime.
 #
+# the class file mentioned is available in the
+# "ChicagoBoyz" folder, or you can compile the
+# Java source from the "java" folder
+#
 # creates the file "cold.html" as offline HTML
 #
 
