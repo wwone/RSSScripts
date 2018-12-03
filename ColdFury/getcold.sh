81898253cd8f2eefd4eca917f2629c0fc632eb56
Bob Swanson <40324245+wwone@users.noreply.github.com>
2018-12-03 15:03:02 -0700

    Create getcold.sh
    First cut, RSS feed for Cold Fury, filters into HTML for offline reading

---
+++ ColdFury/getcold.sh
@@ -0,0 +1,33 @@
+#
+# using "wget" get XML from cold fury feed
+# assume that "wget" is available
+#
+# creates file "tempcold.xml"
+#
+wget -O tempcold.xml http://coldfury.com/index.php/?feed=rss2
+
+JAVA=java
+echo "- - - - - - -"
+echo "- - - - - - -"
+echo "- - - - - - -"
+echo "- - - - - - -"
+
+
+CP=$CP:.
+
+OBJ=ReadXMLDOM
+
+#
+# Java program filters the XML and makes a readable
+# HTML file
+#
+# depends on the presence in this directory of the
+# "ReadXMLDOM.class" file, and the "java" runtime.
+#
+# creates the file "cold.html" as offline HTML
+#
+
+
+echo "$JAVA" -classpath "$CP" $OBJ CI
+"$JAVA" -classpath "$CP" $OBJ  CI <tempcold.xml >cold.html
+
